35800d09d4069e377a7c7dca3340e271861f720a
allpowerful32 <allpowerful32@a8f4b16a-7ba0-f39e-8fda-07da1b298a68>
2010-11-12 05:02:50 +0000

    Basic Application w/ data binding

---
+++ trunk/arae/FileSystemView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace arae
+{
+    class FileSystemView
+    {
+        public List<FileView> Files { get; private set; }
+
+        public List<TagGroup> Tags { get; private set; }
+
+        public List<Specializer> Specializers { get; private set; }
+
+        public FileSystemView()
+        {
+            Files = new List<FileView>();
+            Files.Add(new FileView { Name = "test" });
+            Files.Add(new FileView { Name = "test2" });
+            Files.Add(new FileView { Name = "test3" });
+
+            Tags = new List<TagGroup>();
+            Tags.Add(new TagGroup { Name = "Time" });
+            Tags.Add(new TagGroup { Name = "Favorites" });
+            Tags.Add(new TagGroup { Name = "Locations" });
+        }
+    }
+}
---
+++ trunk/arae/TagGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace arae
+{
+    class TagGroup
+    {
+        public string Name { get; set; }
+
+        public List<Specializer> Tags { get; private set; }
+
+        public TagGroup()
+        {
+            Tags = new List<Specializer>();
+            Tags.Add(new Tag { Name = "test1" });
+            Tags.Add(new Tag { Name = "test2" });
+            Tags.Add(new Tag { Name = "test3" });
+            Tags.Add(new Tag { Name = "test4" });
+            Tags.Add(new Tag { Name = "test5" });
+        }
+    }
+}
