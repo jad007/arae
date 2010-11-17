c8e0404da7fd545f0cb732be33adb48855ce10c9
allpowerful32 <allpowerful32@a8f4b16a-7ba0-f39e-8fda-07da1b298a68>
2010-11-17 15:15:37 +0000

    logic fixes

--- arae/FileSystemView.cs
+++ arae/FileSystemView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace arae
 {
@@ -10,32 +11,30 @@ namespace arae
     {
         public List<FileView> Files { get; private set; }
 
-        public List<TagGroup> Tags { get; private set; }
+        public List<TagGroup> SuggestedTags { get; private set; }
 
         public List<Tag> ActiveTags { get; private set; }
-
-        public Object SelectedItem { get; private set; }
-
-        public List<Specializer> Specializers { get; private set; }
 
         public FileSystemView()
         {
             Files = new List<FileView>();
-            Files.Add(new FileView { Name = "test" });
-            Files.Add(new FileView { Name = "test2" });
-            Files.Add(new FileView { Name = "test3" });
+            Files.Add(new FileView(new FileInfo("test1")));
+            Files.Add(new FileView(new FileInfo("test2")));
+            Files.Add(new FileView(new FileInfo("test3")));
 
-            Tags = new List<TagGroup>();
-            Tags.Add(new TagGroup { Name = "Time" });
-            Tags.Add(new TagGroup { Name = "Favorites" });
-            Tags.Add(new TagGroup { Name = "Locations" });
+            SuggestedTags = new List<TagGroup>();
+            SuggestedTags.Add(new TagGroup { Name = "Time" });
+            SuggestedTags.Add(new TagGroup { Name = "Favorites" });
+            SuggestedTags.Add(new TagGroup { Name = "Locations" });
 
             ActiveTags = new List<Tag>();
             ActiveTags.Add(new Tag { Name = "tag1" });
-            ActiveTags.Add(new Tag { Name = "tag2" });
+            ActiveTags.Add(new Tag { Name = "tag2qweqw" });
             ActiveTags.Add(new Tag { Name = "tag3" });
-
-            SelectedItem = new Tag { Name = "SelectedFolder" };
+            ActiveTags.Add(new Tag { Name = "tag3asdf" });
+            ActiveTags.Add(new Tag { Name = "tag3asdfgd" });
+            ActiveTags.Add(new Tag { Name = "tag3" });
+            ActiveTags.Add(new Tag { Name = "tag3hgdft5r" });
         }
     }
 }
--- arae/FileView.cs
+++ arae/FileView.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
+using System.IO;
 
 namespace arae
 {
     class FileView
     {
         public string Name { get; set; }
+        public Image Icon { get; set; }
+
+        public FileView(FileInfo file)
+        {
+            Name = file.Name;
+            Icon = null;//fixme
+        }
     }
 }
