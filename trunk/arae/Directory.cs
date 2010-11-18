b44b873c5f7d22fd9e610ab0aaa515616a8790fa
duncanj_2007@hotmail.com <duncanj_2007@hotmail.com@a8f4b16a-7ba0-f39e-8fda-07da1b298a68>
2010-11-18 22:48:43 +0000

    Added expanders for directory view.  Also added Directory.cs which was in the solution, but no file was added.  Hopefully I got it mostly right

---
+++ trunk/arae/Directory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace arae
+{
+    class Directory : Specializer
+    {
+        public string Name { get; set; }
+
+        public List<FileView> Files { get; private set; }
+
+        public Directory(String Name)
+        {
+            this.Name = Name;
+
+            Files = new List<FileView>();
+            Files.Add(new FileView(new FileInfo("test1")));
+            Files.Add(new FileView(new FileInfo("test2")));
+            Files.Add(new FileView(new FileInfo("test3")));
+        }
+    }
+}
